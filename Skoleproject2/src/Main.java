5ef2a1a0cff57a08b006ce0c824aae865fcc0a1d
ArthurOS <79909701+faxenarkoman@users.noreply.github.com>
2022-11-24 15:17:54 +0100

    Welcome to Delfinen

--- Skoleproject2/src/Main.java
+++ Skoleproject2/src/Main.java
@@ -1,6 +1,11 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome to Delfinen Choose your account");
+        System.out.println("(1. ---Chairman---)");
+        System.out.println("(2. ---Trainer---)");
+        System.out.println("(3. ---Cashier---)");
+
+
     }
-    System.out.println("Hello world!");
 }
