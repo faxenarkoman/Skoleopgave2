c6ef95e631d2ba9867962525ce2ea9a1ac8f1d13
Jothom2912 <johannes1504@gmail.com>
2022-11-25 13:07:19 +0100

    Nye klasser

--- Skoleproject2/src/Admin.java
+++ Skoleproject2/src/Admin.java
@@ -2,4 +2,9 @@ package Skoleopgave2.Skoleopgave2.Skoleproject2.src;
 
 public class Admin
 {
+    String costumorInfo;
+    int subscriptonInfo;
+    int svimmingResults;
+
+
 }
---
+++ Skoleproject2/src/Swimmer.java
@@ -0,0 +1,8 @@
+package Skoleopgave2.Skoleopgave2.Skoleproject2.src;
+
+public class Swimmer
+{
+    int subscripten;
+    int age;
+    String name;
+}
